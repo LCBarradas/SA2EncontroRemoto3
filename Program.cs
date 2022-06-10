838cf875e0c97968bcee1235d29eb5bcc3f9508c
LCBarradas <100002313+LCBarradas@users.noreply.github.com>
2022-06-10 15:49:05 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using BE5.Classes;
+
+PessoaFisica novaPf = new PessoaFisica();
+
+//novaPf.ValidarDataNascimento (new DateTime (2000,01,01));
+novaPf.ValidarDataNascimento("01/01/2000");
+
